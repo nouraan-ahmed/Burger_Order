fd9886e4c30ee2e89eb60c98782e8e6a1ea05507
nouraan-ahmed <norahahmed1999@gmail.com>
2021-09-12 18:39:50 +0200

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+//import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import { createStore} from 'redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router} from 'react-router-dom'
 import reducer from './store/reducer';
 
 const store = createStore(reducer);
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <Router>
       <App/>
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
 
